1129093b3e30003965c9b632cbaaf942d6fa162e
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-10-04 14:12:47 +0200

    menyfix

--- src/app/[[...slug]]/layout.tsx
+++ src/app/[[...slug]]/layout.tsx
@@ -13,6 +13,49 @@ interface Lenke {
     name: string
     url: string
     mapper: string[]
+    mappe?: string
+}
+
+function LenkeRendring({ lenker }: { lenker: Lenke[] }): ReactElement {
+    const underlenker = lenker
+        .filter((l) => l.mapper.length > 0)
+        .map((l) => {
+            return {
+                ...l,
+                mapper: l.mapper.slice(1),
+                mappe: l.mapper[0],
+            }
+        })
+        .reduce((acc: Map<string, Lenke[]>, obj: Lenke) => {
+            const categoryList = acc.get(obj.mappe!) || []
+            categoryList.push(obj)
+            acc.set(obj.mappe!, categoryList)
+            return acc
+        }, new Map<string, Lenke[]>())
+
+    // grupper undermapper basert på mapper
+
+    return (
+        <>
+            {lenker.map((l, index) => {
+                if (l.mapper.length !== 0) return null
+
+                return (
+                    <AkselLink className="block" underline={false} as={Link} key={index} href={l.url}>
+                        {l.name}
+                    </AkselLink>
+                )
+            })}
+
+            {Array.from(underlenker.keys()).map((k, i) => {
+                return (
+                    <ReadMore header={k} key={i}>
+                        <LenkeRendring lenker={underlenker.get(k)!} />
+                    </ReadMore>
+                )
+            })}
+        </>
+    )
 }
 
 export default async function RootLayout({
@@ -23,66 +66,57 @@ export default async function RootLayout({
 }): Promise<ReactElement> {
     await verifyUserLoggedIn()
     const list = await hentTrelloKort(process.env['TRELLO_BOARD'])
-    const førsteListe = list[0]
-    const deAndreListene = list.slice(1)
+
+    const lenker = [] as Lenke[]
+
+    lenker.push({
+        name: list[0].cards[0].name,
+        url: '/',
+        mapper: [],
+    })
+    lenker.push({
+        name: 'OKR Board',
+        url: '/okr-board/',
+        mapper: [],
+    })
+    list[0].cards.forEach((c, i) => {
+        if (i > 0)
+            lenker.push({
+                name: c.name,
+                url: c.url,
+                mapper: [],
+            })
+    })
+    list.slice(1).forEach((l) => {
+        l.cards.forEach((c) => {
+            lenker.push({
+                name: c.name,
+                url: l.url + '/' + c.url,
+                mapper: [l.name],
+            })
+        })
+    })
+    apper
+        .map((c) => c.name)
+        .sort()
+        .forEach((a) => {
+            lenker.push({
+                name: a,
+                url: '/apper/' + a,
+                mapper: ['Apper'],
+            })
+        })
 
     return (
         <html lang="en">
             <head>
-                <title>{førsteListe.cards[0].name}</title>
+                <title>{list[0].cards[0].name}</title>
                 <meta name="robots" content="noindex" />
             </head>
             <body>
                 <div className="min-h-screen bg-gray-100 flex">
                     <div className="w-[22rem] bg-white py-10 pl-10 pr-5 shadow-md space-y-4">
-                        <AkselLink className="block font-extrabold" underline={false} as={Link} href="/">
-                            {førsteListe.cards[0].name}
-                        </AkselLink>
-                        <AkselLink className="block" underline={false} as={Link} href="/okr-board/">
-                            OKR Board
-                        </AkselLink>
-                        {førsteListe.cards.map((l, index) => {
-                            if (index === 0) return null
-                            return (
-                                <AkselLink className="block" underline={false} as={Link} key={l.id} href={'/' + l.url}>
-                                    {l.name}
-                                </AkselLink>
-                            )
-                        })}
-                        {deAndreListene.map((l) => (
-                            <ReadMore header={l.name} key={l.id}>
-                                {l.cards.map((c) => (
-                                    <AkselLink
-                                        className="block pl-4"
-                                        underline={false}
-                                        as={Link}
-                                        key={l.id}
-                                        href={'/' + l.url + '/' + c.url}
-                                    >
-                                        {c.name}
-                                    </AkselLink>
-                                ))}
-                            </ReadMore>
-                        ))}
-
-                        <ReadMore header="Apper">
-                            <div className=" space-y-2">
-                                {apper
-                                    .map((c) => c.name)
-                                    .sort()
-                                    .map((c, idx) => (
-                                        <AkselLink
-                                            className="block pl-4"
-                                            underline={false}
-                                            as={Link}
-                                            key={idx}
-                                            href={'/' + 'apper' + '/' + c}
-                                        >
-                                            {c}
-                                        </AkselLink>
-                                    ))}
-                            </div>
-                        </ReadMore>
+                        <LenkeRendring lenker={lenker} />
                     </div>
                     <div className="flex-1 max-w-5xl mx-auto p-10">
                         <main>{children}</main>
