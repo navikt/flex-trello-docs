5d74ac3ac33fcbf5448fa73f2e6abf0aab5d67af
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-10-04 16:24:53 +0200

    Edit på github

--- src/components/githubRepoReadme.tsx
+++ src/components/githubRepoReadme.tsx
@@ -1,9 +1,12 @@
 import React, { ReactElement } from 'react'
+import Link from 'next/link'
 
 import { MarkdownAksel } from '@/components/markdownAksel'
+import { AkselLink, EditIcon } from '@/components/clientAksel'
 
 export async function GithubRepoReadme({ repo }: { repo: string }): Promise<ReactElement> {
     const readmeFromGithubRaw = `https://raw.githubusercontent.com/navikt/${repo}/master/README.md`
+    const edit = `https://github.com/navikt/${repo}/edit/master/README.md`
 
     const readmeResponse = await fetch(readmeFromGithubRaw, {
         next: { revalidate: 10 },
@@ -11,5 +14,12 @@ export async function GithubRepoReadme({ repo }: { repo: string }): Promise<Reac
 
     const readme = await readmeResponse.text()
 
-    return <>{readme && <MarkdownAksel md={readme} />}</>
+    return (
+        <>
+            {readme && <MarkdownAksel md={readme} />}
+            <AkselLink as={Link} className="text-gray-500" target="_blank" underline={false} href={edit}>
+                Rediger på GitHub <EditIcon className="inline" title="a11y-title" fontSize="1.5rem" />
+            </AkselLink>
+        </>
+    )
 }
