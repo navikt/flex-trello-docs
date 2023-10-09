03d739f526094465185ea00f0dc772ac3d18fea4
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-10-09 14:15:57 +0200

    Trello bilde fallback

--- src/components/bilde.tsx
+++ src/components/bilde.tsx
@@ -1,17 +1,48 @@
 'use client'
-import { ReactElement } from 'react'
+
+import { Alert } from '@navikt/ds-react'
+import React, { useState, useEffect } from 'react'
 
 export function Bilde(
     props: React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>,
-): ReactElement {
-    return (
-        // eslint-disable-next-line @next/next/no-img-element,jsx-a11y/alt-text
-        <img
-            className="cursor-pointer"
-            {...props}
-            onClick={() => {
-                window.open(props.src, '_blank')
-            }}
-        />
-    )
+): React.ReactElement {
+    const [loaded, setLoaded] = useState<boolean>(false)
+    const [error, setError] = useState<boolean>(false)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (!loaded) {
+                setError(true)
+            }
+        }, 2000)
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [loaded])
+
+    const handleLoad = (): void => {
+        setLoaded(true)
+    }
+
+    const handleError = (): void => {
+        setError(true)
+    }
+
+    const handleClick = (): void => {
+        if (props.src) {
+            window.open(props.src, '_blank')
+        }
+    }
+
+    if (error) {
+        return (
+            <Alert variant="error" className="w-2/3">
+                Feil ved lasting av bilde. Du er nødt til å være pålogget trello i samme browser for å kunne se bilder.
+            </Alert>
+        )
+    }
+
+    // eslint-disable-next-line @next/next/no-img-element,jsx-a11y/alt-text
+    return <img className="cursor-pointer" onLoad={handleLoad} onError={handleError} onClick={handleClick} {...props} />
 }
