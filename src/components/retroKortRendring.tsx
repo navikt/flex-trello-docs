7e497e67b452021b691fe9e0be3e427b9b456a95
golshidbahadorian <golshid.bahadorian@nav.no>
2023-10-10 14:48:20 +0200

    finere retro side

--- src/components/retroKortRendring.tsx
+++ src/components/retroKortRendring.tsx
@@ -8,26 +8,19 @@ import { MarkdownAksel } from '@/components/markdownAksel'
 export function RetroKortRendring({ list }: { list: ListMedCards }): ReactElement {
     return (
         <>
-            <div className="flex justify-center">
-                <Heading size="large" spacing>
-                    {list.name}
-                </Heading>
-            </div>
-            <div className="p-4 bg-gray-100 mt-4">
-                <Heading level="2" size="medium" spacing>
-                    <div className="flex justify-between">Diskusjonspunkter:</div>
-                </Heading>
+            <Heading size="large" spacing>
+                {list.name}
+            </Heading>
+            <Heading className="mt-8" level="2" size="medium" spacing>
+                Diskusjonspunkter:
+            </Heading>
 
-                {list.cards.map((card, index) => (
-                    <div key={index} className="mt-8">
-                        <Heading size="small" level="3">
+            {list.cards.map((card, index) => (
+                <div key={index} className="mt-8 border-b border-gray-300 pb-4">
+                    <div className="mb-4 flex items-center">
+                        <Heading className="mr-4" size="small" level="3">
                             {card.name}{' '}
                         </Heading>
-                        <MarkdownAksel md={card.desc} />
-                        <div>
-                            Ansvarlig:{' '}
-                            {card.members && card.members.length > 0 ? card.members.join(', ') : 'Hele teamet'}
-                        </div>
                         <AkselLink
                             as={Link}
                             className="text-gray-500"
@@ -35,12 +28,14 @@ export function RetroKortRendring({ list }: { list: ListMedCards }): ReactElemen
                             underline={false}
                             href={card.shortUrl}
                         >
-                            {'Sist redigert ' + card.dateLastActivity.slice(0, 10)}{' '}
                             <EditIcon className="inline" title="a11y-title" fontSize="1.5rem" />
                         </AkselLink>
                     </div>
-                ))}
-            </div>
+                    <MarkdownAksel md={card.desc} />
+                    <strong>Ansvarlig:</strong>{' '}
+                    {card.members && card.members.length > 0 ? card.members.join(', ') : 'Hele teamet'}
+                </div>
+            ))}
         </>
     )
 }
