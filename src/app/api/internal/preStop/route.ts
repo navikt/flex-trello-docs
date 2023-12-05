16e1f41fd9bc60d7b32d610ceb266457f94a522a
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-12-05 13:49:04 +0100

    NextResponse

--- src/app/api/internal/preStop/route.ts
+++ src/app/api/internal/preStop/route.ts
@@ -1,7 +1,8 @@
 import { logger } from '@navikt/next-logger'
+import { NextResponse } from 'next/server'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function GET(request: Request): Promise<Response> {
+export async function GET(request: Request): Promise<NextResponse> {
     logger.info('Next.js server: received pre stop request, waiting for 10s before starting shutdown')
     // eslint-disable-next-line
     console.log('Next.js server: received pre stop request console, waiting for 10s before starting shutdown')
@@ -11,5 +12,5 @@ export async function GET(request: Request): Promise<Response> {
         }, 10_000)
     })
     logger.info('Next.js server: starting shutdown')
-    return new Response('ready for shutdown', { status: 200 })
+    return NextResponse.json({ message: 'ready for shutdown' })
 }
