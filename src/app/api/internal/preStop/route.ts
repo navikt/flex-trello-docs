154cdb2966249d9391d473d613a54813ff6509df
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-12-06 09:53:54 +0100

    fix static response

--- src/app/api/internal/preStop/route.ts
+++ src/app/api/internal/preStop/route.ts
@@ -1,16 +1,13 @@
 import { logger } from '@navikt/next-logger'
-import { NextResponse } from 'next/server'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function GET(request: Request): Promise<NextResponse> {
-    logger.info('Next.js server: received pre stop request, waiting for 10s before starting shutdown')
-    // eslint-disable-next-line
-    console.log('Next.js server: received pre stop request console, waiting for 10s before starting shutdown')
+export async function GET(req: Request): Promise<Response> {
+    const text = await req.text()
+    logger.info('Next.js server: received pre stop request, waiting for 10s before starting shutdown ' + text)
     await new Promise<void>((resolve) => {
         setTimeout(() => {
             resolve()
         }, 10_000)
     })
     logger.info('Next.js server: starting shutdown')
-    return NextResponse.json({ message: 'ready for shutdown' })
+    return Response.json({ message: 'ready for shutdown' })
 }
